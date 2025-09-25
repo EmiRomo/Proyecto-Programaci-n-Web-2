63901a3dcf57c78bb768edfa6139e2847eb01942
EmiRomo <emiromormz@gmail.com>
2025-09-25 11:40:56 -0600

    push

--- Front/src/Paginas/InicioSesion.jsx
+++ Front/src/Paginas/InicioSesion.jsx
@@ -2,89 +2,89 @@ import { Footer, Navbar } from "../Componentes/NavFoot";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "../InicioSesion.css"; 
 
 function InicioSesion() {
-  const [correo, setCorreo] = useState("");
-  const [contra, setContra] = useState("");
+  const[correo, setCorreo] = useState("");
+  const[contra, setContra] = useState("");
 
   const redirect = useNavigate();
 
   localStorage.clear();
   localStorage.removeItem("usuario");
-
-  const login = async (event) => {
+  
+  const login = async(event) => {
     event.preventDefault();
 
     try {
-      const respServer = await axios.post("http://localhost:3001/login", {
-        email: correo,
-        pass: contra,
-      });
+
+      const respServer = await axios.post("http://localhost:3001/login", 
+        {
+          email: correo,
+          pass: contra 
+        }
+      )
 
       if (respServer.data.msg === "encontrado") {
+
         localStorage.setItem("usuario", respServer.data.info);
+
         redirect("/Principal");
-      } else if (respServer.data.msg === "noEncontrado") {
+
+      }else if (respServer.data.msg === "noEncontrado") {
+
+
         alert("Usuario no encontrado. Verifique sus datos");
       }
-    } catch (error) {
+
+
+    }catch (error) {
       console.log(error);
       alert("Error al procesar el login");
     }
-  };
+  }
 
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-
-      <header></header>
-
-      {/* Logo fuera del formulario */}
-      <div className="logo">
-        <img src="/imagenes/logo.jpeg" alt="Logo" />
-      </div>
-
-      <div className="container-login">
-        {/* Imagen a la izquierda */}
-        <div className="image-section"></div>
-
-        {/* Formulario a la derecha */}
-        <div className="form-section">
-          <form className="form-box" onSubmit={login}>
-            <h2>¡Bienvenido!</h2>
-
-            <label htmlFor="correo">Usuario:</label>
-            <input
-              type="text"
-              id="correo"
-              name="correo"
-              value={correo}
-              onChange={(e) => setCorreo(e.target.value)}
-            />
-
-            <label htmlFor="password">Contraseña:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={contra}
-              onChange={(e) => setContra(e.target.value)}
-            />
-
-            <button type="submit" className="btn-submit">
-              Entrar
-            </button>
-
-            <div className="forgot-password">
-              <a href="#">¿No estas registrado?</a>
-            </div>
-          </form>
-        </div>
-      </div>
-
-    </div>
-  );
+  return ( 
+   <div className="d-flex flex-column min-vh-100">
+     <Navbar />
+         <div className="container mt-5 flex-grow-1">
+           <h2 className="mb-8 text-center">Inicio Sesion</h2>
+           <form 
+             className="p-4 border rounded shadow-sm bg-light"
+              onSubmit={login}
+           >
+  
+ 
+             <div className="mb-3">
+               <label className="form-label">Correo</label>
+               <input
+                 type="email"
+                 className="form-control"
+                 name="correo"
+                 onChange={(event) => setCorreo(event.target.value) }
+               />
+              
+             </div>
+ 
+             <div className="mb-3">
+               <label className="form-label">Contraseña</label>
+               <input
+                 type="password"
+                 className="form-control"
+                 name="contraseña"
+                 onChange={(event) => setContra(event.target.value) }
+               />
+             </div>
+ 
+ 
+             <button type="submit" className="btn btn-primary w-100">
+               Entrar
+             </button>
+           </form>
+         </div>
+         <Footer />
+       </div>
+       
+     );
 }
 
 export default InicioSesion;
